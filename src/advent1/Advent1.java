335a41db52687a7f8a474f8609dfe82fa279303e
evatherm90 <eva_therm90@hotmail.com>
2019-12-01 22:56:33 +0200

    initial commit

---
+++ src/advent1/Advent1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package advent1;
+
+import java.util.ArrayList;
+import solution.Day1;
+
+/**
+ *
+ * @author antonis
+ */
+public class Advent1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+     Day1 d=new Day1();
+        System.out.println(d.computeTotalFuel());
+        
+        
+        
+    }
+    
+    
+    
+}
